272085abcd138bdc879ea7712186cb7470aa415b
Yong <morristung18102001@gmail.com>
2023-03-25 01:19:49 +0800

    add addToCart function and print them on cart.html

--- assetsBeerLife/script/cartScript.js
+++ assetsBeerLife/script/cartScript.js
@@ -1,4 +1,238 @@
+//This productList is duplocate form productScript.js
+var productList = [
+    {
+        productName:"Anchor",
+        productDesc:"small can",
+        productPrice: "10.00",
+        productInfo: "320ml",
+        imgUrlPath: "assetsBeerLife/img/beer/Anchor_Small_Can.png"
+    },
+    {
+        productName:"Asahi",
+        productDesc:"small can",
+        productPrice: "10.00",
+        productInfo: "320ml",
+        imgUrlPath: "assetsBeerLife/img/beer/Asahi_Small_Can.png"
+    },
+    {
+        productName:"BLANC 1664",
+        productDesc:"small can",
+        productPrice: "10.00",
+        productInfo: "320ml",
+        imgUrlPath: "assetsBeerLife/img/beer/Blanc1664_Small_Can.png"
+    },
+    {
+        productName:"Carlsberg",
+        productDesc:"small can",
+        productPrice: "10.00",
+        productInfo: "320ml",
+        imgUrlPath: "assetsBeerLife/img/beer/Carlsberg_Small_Can.png"
+    },
+    {
+        productName:"Connor's",
+        productDesc:"small can",
+        productPrice: "10.00",
+        productInfo: "320ml",
+        imgUrlPath: "assetsBeerLife/img/beer/Connor's_Small_Can.png"
+    },
+    {
+        productName:"Edelweiss",
+        productDesc:"long can",
+        productPrice: "10.00",
+        productInfo: "330ml",
+        imgUrlPath: "assetsBeerLife/img/beer/Edelweiss_Long_Can.png"
+    },
+    {
+        productName:"Erdinger",
+        productDesc:"long can",
+        productPrice: "10.00",
+        productInfo: "330ml",
+        imgUrlPath: "assetsBeerLife/img/beer/Erdinger_Long_Can.png"
+    },
+    {
+        productName:"Guinness",
+        productDesc:"small can",
+        productPrice: "10.00",
+        productInfo: "320ml",
+        imgUrlPath: "assetsBeerLife/img/beer/Guinness_Small_Can.png"
+    },
+    {
+        productName:"Heineken",
+        productDesc:"small can",
+        productPrice: "10.00",
+        productInfo: "320ml",
+        imgUrlPath: "assetsBeerLife/img/beer/Heineken_Small_Can.png"
+    },
+    {
+        productName:"KirinIchiban",
+        productDesc:"small can",
+        productPrice: "10.00",
+        productInfo: "320ml",
+        imgUrlPath: "assetsBeerLife/img/beer/KirinIchiban_Small_Can.png"
+    },
+    {
+        productName:"RoyalStout",
+        productDesc:"small can",
+        productPrice: "10.00",
+        productInfo: "320ml",
+        imgUrlPath: "assetsBeerLife/img/beer/RoyalStout_Small_Can.png"
+    },
+    {
+        productName:"Sapporo",
+        productDesc:"small can",
+        productPrice: "10.00",
+        productInfo: "320ml",
+        imgUrlPath: "assetsBeerLife/img/beer/Sapporo_Small_Can.png"
+    },
+    {
+        productName:"SingHa",
+        productDesc:"small can",
+        productPrice: "10.00",
+        productInfo: "320ml",
+        imgUrlPath: "assetsBeerLife/img/beer/SingHa_Small_Can.png"
+    },
+    {
+        productName:"SKOL",
+        productDesc:"small can",
+        productPrice: "10.00",
+        productInfo: "320ml",
+        imgUrlPath: "assetsBeerLife/img/beer/SKOL_Small_Can.png"
+    },
+    {
+        productName:"Tiger",
+        productDesc:"small can",
+        productPrice: "10.00",
+        productInfo: "320ml",
+        imgUrlPath: "assetsBeerLife/img/beer/Tiger_Small_Can.png"
+    },
+]
+
 window.addEventListener("load", () => {
+
+    //Print out the item that user was selected.
+    function printItem(target, list) {
+        const listLength = list.length;
+        const container = target;
+    
+        target.innerHTML = " ";
+    
+        if (list.length > 0) {
+            for (let i = 0; i < listLength; i++) {
+
+                //<div class="listItem" id="Guinness"></div>
+                const listItem = document.createElement("div");
+                listItem.classList.add("listItem");
+                listItem.id = list[i].productName;
+                //<div class="imgContainer"></div>
+                const imgContainer = document.createElement("div");
+                imgContainer.classList.add("imgContainer");
+                //<img src="imagePath.png" alt="item photo" class="itemImg">
+                const itemImg = document.createElement("img");
+                itemImg.classList.add("itemImg");
+                itemImg.setAttribute("src", list[i].imgUrlPath);
+                itemImg.setAttribute("alt", "item photo");
+                //<div class="infoContainer"></div>
+                const infoContainer = document.createElement("div");
+                infoContainer.classList.add("infoContainer");
+                //<p class="itemName">Guinness</p>
+                const itemName = document.createElement("p");
+                itemName.classList.add("itemName");
+                itemName.innerText = list[i].productName;
+                //<div class="amountContainer"></div>
+                const amountContainer = document.createElement("div");
+                amountContainer.classList.add("amountContainer");
+                //<p class="itemAmount"></p>
+                const itemAmount = document.createElement("p");
+                itemAmount.classList.add("itemAmount");
+                //<span class="currency">RM</span>
+                const currency1 = document.createElement("span");
+                currency1.classList.add("currency");
+                currency1.innerHTML = "RM";
+                //<input type="text" readonly class="subPrice"value="10.00">
+                const subPrice = document.createElement("input");
+                subPrice.classList.add("subPrice");
+                subPrice.setAttribute("type", "text");
+                subPrice.setAttribute("readonly", true);
+                subPrice.setAttribute("value", list[i].productPrice);
+                //<span class="times">x</span>
+                const times = document.createElement("span");
+                times.classList.add("times");
+                times.innerText = "x";
+                // <input type="number" name="quantity" class="quantity" min="0" value="1" step="1" targetItem="Guinness"></input>
+                const quantity = document.createElement("input");
+                quantity.classList.add("quantity");
+                quantity.setAttribute("type", "number");
+                quantity.setAttribute("name", "quantity");
+                quantity.setAttribute("min", "0");
+                quantity.setAttribute("value", "1");
+                quantity.setAttribute("step", "1");
+                quantity.setAttribute("targetItem", list[i].productName);
+                //<p class="itemPrice"></p>
+                const itemPrice = document.createElement("p");
+                itemPrice.classList.add("itemPrice");
+                //<span class="currency">RM</span>
+                const currency2 = document.createElement("span");
+                currency2.classList.add("currency");
+                currency2.innerHTML = "RM";
+                // <input type="text" readonly value="0.00" class="subTotal">
+                const subTotal = document.createElement("input");
+                subTotal.classList.add("subTotal");
+                subTotal.setAttribute("type", "text");
+                subTotal.setAttribute("readonly", true);
+                subTotal.setAttribute("value", "0.00");
+
+                //Make hierarchy
+                imgContainer.appendChild(itemImg);
+                listItem.appendChild(imgContainer);
+
+                itemAmount.appendChild(currency1);
+                itemAmount.appendChild(subPrice);
+                itemAmount.appendChild(times);
+                itemAmount.appendChild(quantity);
+
+                amountContainer.appendChild(itemAmount);
+
+                itemPrice.appendChild(currency2);
+                itemPrice.appendChild(subTotal);
+                amountContainer.appendChild(itemPrice);
+
+                infoContainer.appendChild(itemName);
+                infoContainer.appendChild(amountContainer);
+                listItem.appendChild(infoContainer);
+        
+                //Add HTML Element into the target
+                container.appendChild(listItem);                
+            }
+        } else {
+            const noItemFound = document.createElement("p");
+            noItemFound.classList.add("noItemFound");
+            noItemFound.innerHTML = "Please select add something to cart from the <a href=\"product.html\">product page</a>."
+            target.appendChild(noItemFound);
+        }
+    }
+
+    function getCartItem(selectedItem) {
+        let tmpList = [];
+        const searchItemList = selectedItem != null ? selectedItem.split("|") : [];
+        if (searchItemList.length > 0) {
+            productList.forEach((element) => {
+                if(searchItemList.includes(element.productName)) {
+                    tmpList.push(element);
+                }
+            })
+        }
+        return tmpList;
+    }
+
+    const searchParam = window.localStorage.getItem("cartItem");
+    const selectedItemList = getCartItem(searchParam);
+
+    const listItemContainer = document.getElementById("listItemContainer");
+
+    printItem(listItemContainer, selectedItemList);
+
+    
+
 
     //Check Valid
     const paymentForm = document.getElementById("paymentForm");
@@ -73,8 +307,9 @@ window.addEventListener("load", () => {
         } else if (parseFloat(document.getElementById("total").value) <= 0) {
             alert("Amount cannot be RM 0!");
         } else {
+            window.localStorage.removeItem("cartItem");
             paymentForm.submit();
-            alert("Submitted");
+            alert("The order was send to us, please wait for the confirmation email. Thank you. 🍺-(￣▽￣o)ノ");
         }
     })
 
--- assetsBeerLife/script/productScript.js
+++ assetsBeerLife/script/productScript.js
@@ -114,105 +114,149 @@ function printProduct(target, list) {
     const listLength = list.length;
     const container = target;
 
-    for (let i = 0; i < listLength; i++) {
-        //Creat html element
+    target.innerHTML = " ";
 
-        //<div class="productItem"></div>
-        const productItem = document.createElement("div");
-        productItem.classList.add("productItem");
-
-        //<div class="imgContainer"></div>
-        const imgContainer = document.createElement("div");
-        imgContainer.classList.add("imgContainer");
-        
-        //<img src="" class="productImg">
-        const productImg = document.createElement("img");
-        productImg.classList.add("productImg");
-        productImg.setAttribute("src", list[i].imgUrlPath);
-
-        //<div class="textContainer"></div>
-        const textContainer = document.createElement("div");
-        textContainer.classList.add("textContainer");
-        
-        //<div class="descContainer"></div>
-        const descContainer = document.createElement("div");
-        descContainer.classList.add("descContainer");
-
-        //<h6 class="productName">Edelweiss</h6>
-        const productName = document.createElement("h6");
-        productName.classList.add("productName");
-        productName.id = list[i].productName;
-        productName.innerText = list[i].productName;
-
-        //<p class="productDesc"></p>
-        const productDesc = document.createElement("p");
-        productDesc.classList.add("productDesc");
-        productDesc.innerText = list[i].productDesc;
-
-
-        //<div class="priceContainer"></div>
-        const priceContainer = document.createElement("div");
-        priceContainer.classList.add("priceContainer");
-
-        //<div class="flexContainer"></div>
-        const flexContainer = document.createElement("div");
-        flexContainer.classList.add("flexContainer");
-
-        //<p class="productPrice"></p>
-        const productPrice = document.createElement("p");
-        productPrice.classList.add("productPrice");
-        
-        //<span class="currency">RM</span>
-        const currency = document.createElement("span");
-        currency.classList.add("currency");
-
-        //<input class="priceAmount" type="text" value="220.00" readonly>
-        const priceAmount = document.createElement("input");
-        priceAmount.classList.add("priceAmount");
-        priceAmount.setAttribute("type", "text");
-        priceAmount.setAttribute("value", list[i].productPrice);
-        priceAmount.setAttribute("readonly", true);
-
-        //<p class="productInfo"></p>
-        const productInfo = document.createElement("p");
-        productInfo.classList.add("productInfo");
-        productInfo.innerText = list[i].productInfo;
-
-        //<button class="addCartBtn">Add to cart</button>
-        const addCartBtn = document.createElement("button");
-        addCartBtn.classList.add("addCartBtn");
-        addCartBtn.innerText = "Add to cart";
-        addCartBtn.onclick = addToCart;
-
-
-        //Make hierarchy
-        imgContainer.appendChild(productImg);
-
-        descContainer.appendChild(productName);
-        descContainer.appendChild(productDesc);
-        textContainer.appendChild(descContainer)
-
-
-        productPrice.appendChild(currency);
-        productPrice.appendChild(priceAmount);
-        flexContainer.appendChild(productPrice);
-        flexContainer.appendChild(productInfo);
-
-        priceContainer.appendChild(flexContainer);
-        priceContainer.appendChild(addCartBtn);
-        textContainer.appendChild(priceContainer);
-
-        productItem.appendChild(imgContainer);
-        productItem.appendChild(textContainer);
-
-
-
-
-        //Add HTML Element into the target
-        container.appendChild(productItem);                
+    if (list.length > 0) {
+        for (let i = 0; i < listLength; i++) {
+            //Creat html element
+    
+            //<div class="productItem"></div>
+            const productItem = document.createElement("div");
+            productItem.classList.add("productItem");
+    
+            //<div class="imgContainer"></div>
+            const imgContainer = document.createElement("div");
+            imgContainer.classList.add("imgContainer");
+            
+            //<img src="" class="productImg">
+            const productImg = document.createElement("img");
+            productImg.classList.add("productImg");
+            productImg.setAttribute("src", list[i].imgUrlPath);
+    
+            //<div class="textContainer"></div>
+            const textContainer = document.createElement("div");
+            textContainer.classList.add("textContainer");
+            
+            //<div class="descContainer"></div>
+            const descContainer = document.createElement("div");
+            descContainer.classList.add("descContainer");
+    
+            //<h6 class="productName">Edelweiss</h6>
+            const productName = document.createElement("h6");
+            productName.classList.add("productName");
+            productName.id = list[i].productName;
+            productName.innerText = list[i].productName;
+    
+            //<p class="productDesc"></p>
+            const productDesc = document.createElement("p");
+            productDesc.classList.add("productDesc");
+            productDesc.innerText = list[i].productDesc;
+    
+    
+            //<div class="priceContainer"></div>
+            const priceContainer = document.createElement("div");
+            priceContainer.classList.add("priceContainer");
+    
+            //<div class="flexContainer"></div>
+            const flexContainer = document.createElement("div");
+            flexContainer.classList.add("flexContainer");
+    
+            //<p class="productPrice"></p>
+            const productPrice = document.createElement("p");
+            productPrice.classList.add("productPrice");
+            
+            //<span class="currency">RM</span>
+            const currency = document.createElement("span");
+            currency.classList.add("currency");
+    
+            //<input class="priceAmount" type="text" value="220.00" readonly>
+            const priceAmount = document.createElement("input");
+            priceAmount.classList.add("priceAmount");
+            priceAmount.setAttribute("type", "text");
+            priceAmount.setAttribute("value", list[i].productPrice);
+            priceAmount.setAttribute("readonly", true);
+    
+            //<p class="productInfo"></p>
+            const productInfo = document.createElement("p");
+            productInfo.classList.add("productInfo");
+            productInfo.innerText = list[i].productInfo;
+    
+            //<button class="addCartBtn">Add to cart</button>
+            const addCartBtn = document.createElement("button");
+            addCartBtn.classList.add("addCartBtn");
+            addCartBtn.innerText = "Add to cart";
+            addCartBtn.addEventListener("click", (e) => {
+                addToCart(list[i].productName);
+            })
+    
+    
+            //Make hierarchy
+            imgContainer.appendChild(productImg);
+    
+            descContainer.appendChild(productName);
+            descContainer.appendChild(productDesc);
+            textContainer.appendChild(descContainer)
+    
+    
+            productPrice.appendChild(currency);
+            productPrice.appendChild(priceAmount);
+            flexContainer.appendChild(productPrice);
+            flexContainer.appendChild(productInfo);
+    
+            priceContainer.appendChild(flexContainer);
+            priceContainer.appendChild(addCartBtn);
+            textContainer.appendChild(priceContainer);
+    
+            productItem.appendChild(imgContainer);
+            productItem.appendChild(textContainer);
+    
+    
+    
+    
+            //Add HTML Element into the target
+            container.appendChild(productItem);                
+        }
+    } else {
+        const noProductFound = document.createElement("p");
+        noProductFound.classList.add("noProductFound");
+        noProductFound.innerHTML = "No similar product."
+        target.appendChild(noProductFound);
     }
 }
+
+function searchItem(seachParam) {
+    if (seachParam !== null) {
+        var searchResultList = [];
+        productList.forEach((element) => {
+            if(element.productName.toLowerCase().includes(seachParam.toLowerCase())) {
+                searchResultList.push(element);
+            }
+        });
+        productList = searchResultList;
+    }
+}
+
+function addToCart(itemName) {
+    console.log(itemName);
+    const oldData = localStorage.getItem("cartItem");
+    let newData = "";
+    if (oldData != null) {
+        if (!oldData.includes(itemName)) {
+            newData = `${oldData}|${itemName}`;
+            localStorage.setItem("cartItem", newData);
+        }
+    } else {
+        newData = itemName;
+        localStorage.setItem("cartItem", newData);
+    }
+    alert("The item was added to the cart. Kindly change the quantity on the payment page.");
+}
+
+
 window.addEventListener("load", () => {
     const productContainer = document.getElementById("productContainer");
+    const seachValue = new URLSearchParams(window.location.search);
+
+    searchItem(seachValue.get("search"));
     printProduct(productContainer, productList);
 })
